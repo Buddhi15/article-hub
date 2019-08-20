c29e6354411a320983adf644c6871bc66afb598e
sanduni <sandunipavithra9611@gmail.com>
2019-08-20 19:51:13 +0530

    Issue#2-Create connection String

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Connection String
+const mongoose = require('mongoose');
+const connection = mongoose.connect('mongodb+srv://buddhi15:eg121947@articlehub-8en2d.mongodb.net/test?retryWrites=true&w=majority', {useNewUrlParser: true});
+connection .then((db) => { console.log("Connected correctly to server"); }) .catch((err) => { console.log(err) });
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
